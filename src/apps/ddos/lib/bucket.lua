2fe3a1e87253949f7a2bf473bd306b4639b22f9d
Ben Agricola <bagricola@squiz.co.uk>
2016-05-03 16:45:00 +0100

    Print bucket status
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/apps/ddos/lib/bucket.lua
+++ src/apps/ddos/lib/bucket.lua
@@ -215,8 +215,8 @@ function Bucket:stop()
 end
 
 function Bucket:status()
-    local msg = "%s: %d/%d pps - %d/%d bps - Totals: %d packets / %d Mbits"
-    log_debug(msg, self.name, self:get_counter('pps'), self.pps_rate or self.pps_burst_rate or 0, self:get_counter('bps'), self.bps_rate or self.bps_burst_rate or 0, self:get_counter('total_packets'), (self:get_counter('total_bits') / 1024 / 1024))
+    local msg = "%s [%s]: %d/%d pps - %d/%d bps - Totals: %d packets / %d Gbytes"
+    log_debug(msg, self.name, self.violated:upper() or "OK", self:get_counter('pps'), self.pps_rate or self.pps_burst_rate or 0, self:get_counter('bps'), self.bps_rate or self.bps_burst_rate or 0, self:get_counter('total_packets'), (self:get_counter('total_bits') / 8589934592))
 end
 
 return Bucket
