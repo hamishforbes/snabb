5444b442ff1c203904ad6abae1eedc01c71c7fad
Ben Agricola <bagricola@squiz.co.uk>
2016-05-03 17:09:45 +0100

    Fix bits to bytes
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/apps/ddos/lib/bucket.lua
+++ src/apps/ddos/lib/bucket.lua
@@ -215,8 +215,8 @@ function Bucket:stop()
 end
 
 function Bucket:status()
-    local msg = "%s [%s]: %d/%d pps - %d/%d bps - Totals: %d Kpackets / %d Gbytes"
-    log_debug(msg, self.name, self.violated or "OK", self:get_counter('pps'), self.pps_rate or self.pps_burst_rate or 0, self:get_counter('bps'), self.bps_rate or self.bps_burst_rate or 0, self:get_counter('total_packets') / 1000, (self:get_counter('total_bits') / 8589934592))
+    local msg = "%s [%s]: %d/%d pps - %d/%d bps - Totals: %d Kpackets / %d Mbytes"
+    log_debug(msg, self.name, self.violated or "OK", self:get_counter('pps'), self.pps_rate or self.pps_burst_rate or 0, self:get_counter('bps'), self.bps_rate or self.bps_burst_rate or 0, self:get_counter('total_packets') / 1000, (self:get_counter('total_bits') / 8388608))
 end
 
 return Bucket
