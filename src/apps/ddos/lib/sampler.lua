ee9b3d77eed2bf29e1f0eaa35827495cf3d7e0e0
Ben Agricola <bagricola@squiz.co.uk>
2016-05-11 12:11:59 +0100

    Add copy?
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/apps/ddos/lib/sampler.lua
+++ src/apps/ddos/lib/sampler.lua
@@ -108,7 +108,8 @@ local function int_to_dotted(num)
 end
 
 local function get_ipv4(offset, mask)
-    local in_addr = ffi_cast('int32_t*', offset)
+    local in_addr = ffi_typeof('uint32_t')
+    ffi_copy(in_addr, offset, 4)
     in_addr = bit_band(in_addr, mask)
     return in_addr
 end
