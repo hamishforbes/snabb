5422bb7d5d3352b282255ef7242c0f708b4c197e
Ben Agricola <bagricola@squiz.co.uk>
2016-05-11 15:01:11 +0100

    Remove double appended mask
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/apps/ddos/lib/sampler.lua
+++ src/apps/ddos/lib/sampler.lua
@@ -301,8 +301,8 @@ function SampleSet:sample(p)
         self.dst_hosts:value(dst_ip)
 
         -- Parse src and dst subnets based on a mask
-        local src_subnet = get_ipv4_src(e_payload, subnet_mask) .. "/24"
-        local dst_subnet = get_ipv4_dst(e_payload, subnet_mask) .. "/24"
+        local src_subnet = get_ipv4_src(e_payload, subnet_mask)
+        local dst_subnet = get_ipv4_dst(e_payload, subnet_mask)
 
         self.src_subnets:value(src_subnet)
         self.dst_subnets:value(dst_subnet)
