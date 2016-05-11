6306f0565f6e7c6fdecc25ce1f63689d2ee70541
Ben Agricola <bagricola@squiz.co.uk>
2016-05-11 17:59:11 +0100

    Save filter in bucket for info
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/apps/ddos/lib/bucket.lua
+++ src/apps/ddos/lib/bucket.lua
@@ -44,8 +44,8 @@ end
 
 function Bucket:new(cfg)
     local self = {
-        name = cfg.name,
-        cfg  = cfg,
+        name    = cfg.name,
+        filter  = cfg.filter,
         period         = cfg.period or 1,  -- Buckets are 5s long
         average_period = cfg.avg_period or 30, -- EWMA is calculated over 30s
         pps_burst_rate = cfg.pps_burst_rate,
