6f82d06bc133ef80ea987590d7fb591fbe5ea604
Ben Agricola <bagricola@squiz.co.uk>
2016-07-08 10:05:49 +0100

    Set master
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- src/program/radish_profiler/radish_profiler.lua
+++ src/program/radish_profiler/radish_profiler.lua
@@ -133,6 +133,7 @@ function run (args)
         log_info("Input interface %s is physical device, initialising...", opt.int_in)
         config.app(c, "int_in", driver.driver, {
             pciaddr = dev.pciaddress,
+            master  = true,
         })
     end
 
